2032cb1b6f4ec363a3b748c26921530fed1576c5
Satyam Srivastava <52953724+SatyamSrivastava7@users.noreply.github.com>
2026-01-14 03:26:34 +0000

    update

--- server/db.ts
+++ server/db.ts
@@ -12,7 +12,7 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  max: 1,
+  max: 3,
   idleTimeoutMillis: 60000,
   connectionTimeoutMillis: 10000,
 });
